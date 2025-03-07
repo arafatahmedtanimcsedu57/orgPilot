80248d80e86ae957432f06b401117efcde756f80
Arafat <arafatahmedtanimcsedu57@gmail.com>
2025-03-08 00:13:44 +0600

    update

--- components/ui/status-badge.tsx
+++ components/ui/status-badge.tsx
@@ -2,15 +2,15 @@ import { Badge } from './badge';
 
 const StatusBadge = ({ type }: { type: string }) => {
 	const classNames: Record<any, string> = {
-		ACTIVE: 'bg-green-600/20 text-green-600 border border-green-700',
-		INACTIVE: 'bg-red-600/20 text-red-700 border border-red-700',
+		ACTIVE: 'bg-green-600/40 text-green-950 !border-0',
+		INACTIVE: 'bg-red-600/40 text-rose-950 !border-0',
 		DEFAULT: '',
 	} as const;
 
 	return (
 		<Badge
 			variant={'outline'}
-			className={`hover:bg px-4 py-2 text-xs font-normal leading-[0] w-max ${classNames[type]}`}
+			className={`hover:bg px-4 py-3 text-xs font-semibold leading-[0] w-max ${classNames[type]}`}
 		>
 			{type}
 		</Badge>
